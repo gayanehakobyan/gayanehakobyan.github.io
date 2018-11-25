5e3038adfea87ea7e090001fd7e96dcfee9e458c
gayanehakobyan <gayanehakobyan93@gmail.com>
2018-11-25 14:32:07 +0400

    My page

---
+++ main.js
@@ -0,0 +1,43 @@
+window.onload=function() {
+	window.scrollTo(0,0);
+	console.log("sdsdas")
+}
+
+// let div1=document.getElementById("div1")
+// window.addEventListener("scroll", function(){
+// 	let offset=window.pageYOffset;
+// 	div1.style.backgroundPositionY=offset*0.7+"px"
+// })
+// let div2=document.getElementById("div1")
+// window.addEventListener("scroll", function(){
+// 	let offset=window.pageYOffset;
+// 	div2.style.backgroundPositionY=offset*0.7+"px"
+// })
+// let div3=document.getElementById("div1")
+// window.addEventListener("scroll", function(){
+// 	let offset=window.pageYOffset;
+// 	div3.style.backgroundPositionY=offset*0.7+"px"
+// })
+// let div4=document.getElementById("div1")
+// window.addEventListener("scroll", function(){
+// 	let offset=window.pageYOffset;
+// 	div4.style.backgroundPositionY=offset*0.7+"px"
+// })
+// let div5=document.getElementById("div1")
+// window.addEventListener("scroll", function(){
+// 	let offset=window.pageYOffset;
+// 	div5.style.backgroundPositionY=offset*0.7+"px"
+// });
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+//     	console.log("if true")
+// 	logoImage.style.display="block"
+//     } else {
+//     	console.log('if false')
+// 	logoImage.style.display="none"
+//     }
+// }
+
